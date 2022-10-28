4b1eeed3c8917699b8d4d30fc213956294cd6c7c
iuryflores <iury.flores@gmail.com>
2022-10-27 22:17:28 -0300

    Fix title of Schema, to add require

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,7 @@ import { Schema as _Schema, model } from 'mongoose';
 const Schema = _Schema;
 
 const recipeSchema = new Schema({
-  title: { type: String, unique },
+  title: { type: String, unique, require },
   level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] },
   ingredients: [String],
   cuisine: { type: String, require },
